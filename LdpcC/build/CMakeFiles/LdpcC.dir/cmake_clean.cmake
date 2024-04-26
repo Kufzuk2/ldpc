28a88336286062b3c98f82fd3bde13bc499ef875
KufzuK <vologin.vv@phystech.edu>
2024-04-26 16:33:15 +0300

    transmission of the project to github

---
+++ LdpcC/build/CMakeFiles/LdpcC.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LdpcC.dir/Constellation.cpp.o"
+  "CMakeFiles/LdpcC.dir/Constellation.cpp.o.d"
+  "CMakeFiles/LdpcC.dir/LdpcCode.cpp.o"
+  "CMakeFiles/LdpcC.dir/LdpcCode.cpp.o.d"
+  "CMakeFiles/LdpcC.dir/main.cpp.o"
+  "CMakeFiles/LdpcC.dir/main.cpp.o.d"
+  "LdpcC"
+  "LdpcC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LdpcC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
